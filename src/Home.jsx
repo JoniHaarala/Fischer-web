0daf521fbec0869621b053a3b7056372279f0421
jonatan <jonatan.haaralaorosco@gmail.com>
2022-11-08 20:19:06 -0300

    disabled search tab menue

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import mainbg from './img/mainbg.webp'
-import TabMenu from './components/tabPanel'
 import Services from './components/Services'
 import Comments from './components/Comments'
 import Destacadas from './components/Destacadas'
@@ -15,17 +14,10 @@ export default function Home() {
                 alt='fondo de bienvenida de pagina'
                 className="mx-1 object-cover h-[280px] md:mx-0 md:w-[2300px] md:h-[600px]"
             />
-            <div className='flex justify-center'>
-                <div className="w-fit h-fit bg-green-400 p-5 mx-2 rounded-lg md:mx-0 md:my-20 font-bold">
-                    <h1 className='flex justify-center'>Realice una busqueda rapida</h1>
-                    <TabMenu />
-                </div>
-            </div>
             {/* tarjetas de freatures de la empresa */}
             <Services />
             {/* propiedades destacadas */}
             <Destacadas />
-            <section className="h-52"><h1>holahola</h1></section>
             {/* tarjetas de testimoniales */}
             <Comments />
         </div>
